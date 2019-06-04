78df700eec32e8a3cb77cf7544432a52cc6a856d
jarrodfolino <jdfolino@gmail.com>
2019-06-04 22:16:12 +1000

    dd

--- services/website-service/src/publish.sh
+++ services/website-service/src/publish.sh
@@ -1,8 +1,8 @@
 IMAGE_REPO_NAME="jdfolino"
-IMAGE_TAG="products"
+IMAGE_TAG="website"
 AWS_ACCOUNT_ID="192078575311"
 AWS_DEFAULT_REGION="us-east-1"
-BUILD_ID="kwxkbwkbxkw323nm2emn3mn"
+BUILD_ID="efcvwoecleslxlks"
 
 docker build -t $IMAGE_REPO_NAME:$IMAGE_TAG .
 docker tag $IMAGE_REPO_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$BUILD_ID
